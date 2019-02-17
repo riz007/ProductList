c22d01bcfc0f5bf83491349c3bca128f429a3a5c
Rizwanul Islam Rudra <rizwanul.ru@chilindo.com>
2019-02-17 16:06:16 +0700

    Added vuejs files and list.json

---
+++ src/components/ItemList.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="row">
+    <div class="col-12 col-sm-6 col-md-4 col-lg-3" v-for="item in items" :key="item.id">
+       <div class="card mt-3">
+        <img v-bind:src="item.image_url" alt="Item cover image" class="card-img-top">
+        <div class="card-body">
+            <p class="card-title"><a href="#" data-toggle="tooltip" data-placement="top" v-bind:title="item.title">{{item.title}}</a></p>
+            <p class="card-text">{{item.created_at}}</p>
+            <div class="row">
+                <div class="col">
+                    <p class="">{{item.price | toTHB}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+  
+</div>
+</template>
+
+<script>
+export default {
+    name: 'ItemList',
+    props: ["items"]
+}    
+</script>
+
+<style scoped>
+
+.card-title {
+    white-space:pre;
+    overflow:hidden;
+    text-overflow: ellipsis;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="container">
+        <ItemList :items="items"></ItemList>
+        <div class="row">
+            <div class="col-12">
+                <Pagination />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import ItemList from "./ItemList"
+import Pagination from "./Pagination"
+import axios from "axios"
+import Vue from "vue"
+
+Vue.filter('toTHB', function (value) {
+    return `฿${value}`;
+});
+
+export default {
+    components: {
+        ItemList,
+        Pagination
+    },
+    data() {
+        return {
+            items: [],
+            errors: []
+        }
+    },
+    created() {
+        axios.get('list.json')
+        .then(response => {
+        // JSON responses are automatically parsed.
+        this.items = response.data;
+        })
+        .catch(e => {
+        this.errors.push(e);
+        // console.log('error');
+        })
+    }
+
+}
+</script>
